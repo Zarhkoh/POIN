b043deed085512ca80c61e632e083ee7999a720e
jthery <j.thery2311@outlook.fr>
2020-04-27 14:00:24 +0200

    test on index.html

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,38 +7,38 @@ import * as handTrack from 'handtrackjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
-  @ViewChild('myCanvas', {static: false}) myCanvas: ElementRef;
-  public context: CanvasRenderingContext2D;
-  img = document.getElementById('img');
 
-  modelParams = {
-    flipHorizontal: true,   // flip e.g for video 
-    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
-    maxNumBoxes: 20,        // maximum number of boxes to detect
-    iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.79,    // confidence threshold for predictions.
-  }
+  // @ViewChild('myCanvas', {static: false}) myCanvas: ElementRef;
+  // public context: CanvasRenderingContext2D;
+  // img = document.getElementById('img');
+
+  // modelParams = {
+  //   flipHorizontal: true,   // flip e.g for video 
+  //   imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+  //   maxNumBoxes: 20,        // maximum number of boxes to detect
+  //   iouThreshold: 0.5,      // ioU threshold for non-max suppression
+  //   scoreThreshold: 0.79,    // confidence threshold for predictions.
+  // }
 
   constructor() { }
 
   ngOnInit() {
-    this.loadIMG();
+    // this.loadIMG();
   }
 
-    loadIMG() {
-    // Load the model.
-    handTrack.load(this.modelParams).then(model => {
-      // detect objects in the image.
-      console.log("model loaded")
-      model.detect(this.img).then(predictions => {
-        console.log('Predictions: ', predictions);
-      });
-    });
-  }
+  //   loadIMG() {
+  //   // Load the model.
+  //   handTrack.load(this.modelParams).then(model => {
+  //     // detect objects in the image.
+  //     console.log("model loaded")
+  //     model.detect(this.img).then(predictions => {
+  //       console.log('Predictions: ', predictions);
+  //     });
+  //   });
+  // }
 
-  ngAfterViewInit(): void {
-    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
-  }
+  // ngAfterViewInit(): void {
+  //   this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+  // }
 
 }
