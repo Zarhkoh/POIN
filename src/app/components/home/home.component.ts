fb4003f93b73b1bfd9317f92b4ab8c3db47e383b
jthery <j.thery2311@outlook.fr>
2020-04-27 11:45:10 +0200

    in progress handtrackjs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as handTrack from 'handtrackjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  @ViewChild('myCanvas', {static: false}) myCanvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+  img = document.getElementById('img');
+
+  modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.loadIMG();
+  }
+
+    loadIMG() {
+    // Load the model.
+    handTrack.load(this.modelParams).then(model => {
+      // detect objects in the image.
+      console.log("model loaded")
+      model.detect(this.img).then(predictions => {
+        console.log('Predictions: ', predictions);
+      });
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
   }
 
 }
